7a711b8349595c950c37e0aae260e5ab97054b11
Eslam-Nawara <eslamnawara44@gmailcom>
2022-07-28 13:35:53 +0200

    Add the domo and postman tests

---
+++ demo/client.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"net/http"
+)
+
+const (
+	Req           = `{"id": %d, "item": "task id %d", "completed": %s}`
+	ServerAddress = "http://localhost:5000%s"
+
+	MethodGet     = "GET"
+	MethodHead    = "HEAD"
+	MethodPost    = "POST"
+	MethodPut     = "PUT"
+	MethodPatch   = "PATCH"
+	MethodDelete  = "DELETE"
+	MethodConnect = "CONNECT"
+	MethodOptions = "OPTIONS"
+	MethodTrace   = "TRACE"
+)
+
+func main() {
+	client := &http.Client{}
+
+	// Adding new task
+	fmt.Println("*********Add new tasks*********")
+	for i := 1; i <= 10; i++ {
+		body := fmt.Sprintf(Req, i, i, "false")
+		req := buildRequest("POST", "/todo", []byte(body))
+		res, _ := client.Do(req)
+		printResponse(res)
+	}
+
+	// Get all tasks
+	fmt.Println("*********Get all tasks*********")
+	req := buildRequest(MethodGet, "/todo", []byte(""))
+	res, _ := client.Do(req)
+	printResponse(res)
+
+	// Get task by ID
+	fmt.Println("*********Get task by ID = 1*********")
+	req = buildRequest(MethodGet, "/todo/1", []byte(""))
+	res, _ = client.Do(req)
+	printResponse(res)
+
+	// Update task by ID
+	fmt.Println("*********Update task with ID = 1*********")
+	body := fmt.Sprintf(Req, 1, 1, "true")
+	req = buildRequest(MethodPatch, "/todo", []byte(body))
+	res, _ = client.Do(req)
+	printResponse(res)
+
+	// check task completed by ID
+	fmt.Println("*********Check task with ID = 1*********")
+	req = buildRequest(MethodPatch, "/todo/1", []byte(""))
+	res, _ = client.Do(req)
+	printResponse(res)
+
+	// Delete task by ID
+	fmt.Println("*********Delete task by ID = 1*********")
+	req = buildRequest(MethodDelete, "/todo/1", []byte(""))
+	res, _ = client.Do(req)
+	printResponse(res)
+
+	fmt.Println("*********Delete task by ID = 2*********")
+	req = buildRequest(MethodDelete, "/todo/2", []byte(""))
+	res, _ = client.Do(req)
+	printResponse(res)
+
+	fmt.Println("*********Delete task by ID = 3*********")
+	req = buildRequest(MethodDelete, "/todo/3", []byte(""))
+	res, _ = client.Do(req)
+	printResponse(res)
+
+	// Get all tasks
+	fmt.Println("*********Get all tasks*********")
+	req = buildRequest(MethodGet, "/todo", []byte(""))
+	res, _ = client.Do(req)
+	printResponse(res)
+}
+
+func buildRequest(method, endpoint string, body []byte) *http.Request {
+	serverAddress := fmt.Sprintf(ServerAddress, endpoint)
+	req, _ := http.NewRequest(method, serverAddress, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	return req
+}
+func printResponse(res *http.Response) {
+	defer res.Body.Close()
+	fmt.Println("Response status:", res.Status)
+	scanner := bufio.NewScanner(res.Body)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	fmt.Println()
+}
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 var db *gorm.DB
 
 type Task struct {
-	ID        string `gorm:"NOT NULL" json:"id"`
+	ID        int64  `gorm:"NOT NULL" json:"id"`
 	Item      string `json:"item"`
 	Completed bool   `jon:"completed"`
 }
@@ -45,26 +45,26 @@ func OpenDB(path string) error {
 }
 
 func SetRoutes(router *mux.Router) {
-	router.HandleFunc("/", GetTasks).Methods(("GET"))
 	router.HandleFunc("/todo", GetTasks).Methods("GET")
 	router.HandleFunc("/todo", AddTask).Methods("POST")
+	router.HandleFunc("/todo", UpdateTask).Methods("PATCH")
 	router.HandleFunc("/todo/{id}", GetTask).Methods("GET")
 	router.HandleFunc("/todo/{id}", CompleteTask).Methods("PATCH")
 	router.HandleFunc("/todo/{id}", DeleteTask).Methods("DELETE")
-	router.HandleFunc("/todo/{id}/{item}", UpdateTask).Methods("PATCH")
 }
 
 func GetTasks(writer http.ResponseWriter, request *http.Request) {
 	var tasks []Task
 	db.Find(&tasks)
-	jsonData, _ := json.Marshal(tasks)
-	writer.WriteHeader(http.StatusAccepted)
+	jsonData, _ := json.MarshalIndent(tasks, "", "  ")
+	request.Header.Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
 	writer.Write(jsonData)
 }
 
 func AddTask(writer http.ResponseWriter, request *http.Request) {
 	var newTask Task
-	if json.NewDecoder(request.Body).Decode(&newTask) != nil {
+	if json.NewDecoder(request.Body).Decode(&newTask) != nil || validateData(newTask) != nil {
 		http.Error(writer, errInvalidInput, http.StatusBadRequest)
 		return
 	}
@@ -73,8 +73,9 @@ func AddTask(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 	db.Create(&newTask)
-	jsonData, _ := json.Marshal(newTask)
-	writer.WriteHeader(http.StatusAccepted)
+	jsonData, _ := json.MarshalIndent(newTask, "", "  ")
+	request.Header.Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
 	writer.Write(jsonData)
 }
 
@@ -82,7 +83,7 @@ func GetTask(writer http.ResponseWriter, request *http.Request) {
 	params := mux.Vars(request)
 	id, err := strconv.Atoi(params["id"])
 	if err != nil {
-		http.Error(writer, err.Error(), http.StatusBadRequest)
+		http.Error(writer, errInvalidInput, http.StatusBadRequest)
 		return
 	}
 	var requestedTask Task
@@ -90,7 +91,8 @@ func GetTask(writer http.ResponseWriter, request *http.Request) {
 		http.Error(writer, errNotFound, http.StatusNotFound)
 		return
 	}
-	jsonData, _ := json.Marshal(requestedTask)
+	request.Header.Set("Content-Type", "application/json")
+	jsonData, _ := json.MarshalIndent(requestedTask, "", "  ")
 	writer.WriteHeader(http.StatusAccepted)
 	writer.Write(jsonData)
 
@@ -107,6 +109,7 @@ func DeleteTask(writer http.ResponseWriter, request *http.Request) {
 		http.Error(writer, errNotFound, http.StatusNotFound)
 		return
 	}
+	request.Header.Set("Content-Type", "application/json")
 	writer.WriteHeader(http.StatusAccepted)
 	writer.Write([]byte("Task deleted successfully"))
 }
@@ -125,26 +128,41 @@ func CompleteTask(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 	checkedTask.Completed = !checkedTask.Completed
-	writer.WriteHeader(http.StatusAccepted)
 	db.Save(checkedTask)
+	request.Header.Set("Content-Type", "application/json")
+	jsonData, _ := json.MarshalIndent(checkedTask, "", "  ")
+	writer.WriteHeader(http.StatusAccepted)
+	writer.Write(jsonData)
 
 }
 
 func UpdateTask(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	id, err := strconv.Atoi(params["id"])
-	item, _ := params["item"]
-	if err != nil {
+	var task Task
+	if json.NewDecoder(request.Body).Decode(&task) != nil || validateData(task) != nil {
 		http.Error(writer, errInvalidInput, http.StatusBadRequest)
 		return
 	}
+	id := task.ID
+	item := task.Item
+
 	updatedTask := Task{}
 	if db.First(&updatedTask, id).Error != nil {
-		http.Error(writer, "Bad request, task already exist", http.StatusConflict)
+		http.Error(writer, errNotFound, http.StatusConflict)
 		return
 	}
 	updatedTask.Item = item
-	writer.WriteHeader(http.StatusAccepted)
 	db.Save(updatedTask)
+	request.Header.Set("Content-Type", "application/json")
+	jsonData, _ := json.MarshalIndent(updatedTask, "", "  ")
+	writer.WriteHeader(http.StatusAccepted)
+	writer.Write(jsonData)
 
+}
+
+func validateData(task Task) error {
+
+	if task.Item == "" || task.ID <= 0 {
+		return fmt.Errorf("Invalid task")
+	}
+	return nil
 }
